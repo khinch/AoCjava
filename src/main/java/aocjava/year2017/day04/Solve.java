1ab8aeb34c5238a07e57bbcc52b0cbba5c1367ba
Kieren Hinch <khinch-github@nylonmoon.com>
2018-08-13 07:38:41 +0100

    Added solve class and unit tests.

---
+++ src/main/java/aocjava/year2017/day04/Solve.java
@@ -0,0 +1,25 @@
+package aocjava.year2017.day04;
+
+
+import aocjava.InvalidPuzzleInputException;
+
+/**
+ * Class to solve puzzle on 4th Dec 2017
+ *
+ * http://adventofcode.com/2017/day/4
+ *
+ * TODO: Implement logging
+ */
+
+public class Solve
+{
+    public static String part1(String input) throws InvalidPuzzleInputException
+    {
+        return "not implemented";
+    }
+
+    public static String part2(String input) throws InvalidPuzzleInputException
+    {
+        return "not implemented";
+    }
+}
